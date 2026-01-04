cf36b4cb88c4152ed7f656e17acca0f42d51392d
Nazarii Konechnyi <nazar.konechniy2@gmail.com>
2026-01-04 03:01:27 +0000

    deploy fix

--- server/main.py
+++ server/main.py
@@ -1,20 +1,26 @@
+import os
 from fastapi import FastAPI
-from src.api.main import api_router
+from fastapi.staticfiles import StaticFiles
+from src.api.main import api_router 
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-import os
 
 app = FastAPI(title="My Chess")
 app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
+frontend_path = os.path.join(os.path.dirname(__file__), "../client/dist")
+print("Serving frontend from:", frontend_path)
+
+app.mount("/", StaticFiles(directory=frontend_path, html=True), name="frontend")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("server.main:app", host="0.0.0.0", port=port)
